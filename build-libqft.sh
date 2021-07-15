b39607c50ff03b0a1b79211c9f8bc5275b676ad5
xiaohui_gao <waky_7635@126.com>
2021-07-15 20:51:32 +0800

    buid for windows

--- build-libqft.sh
+++ build-libqft.sh
@@ -65,6 +65,8 @@ case $Num in
   -lz -lstdc++ -lm
 ;;
 3)
+  export THIRD_PARTY_PATH=/mingw32/i686-w64-mingw32
+  export QSVINC=$THIRD_PARTY_PATH/mingw32/include
   echo "build for windows"
   gcc -o ./libqft.dll -shared -fPIC \
 	-Wl,--output-def,./libhcsvcapi.def,--out-implib,./libhcsvcapi.lib \
@@ -82,13 +84,18 @@ case $Num in
   ./source/file_pkt2raw.c \
   ./source/base_stun.c \
   ./source/stun_list.c \
+  -I/D/msys64/mingw32/include \
+	-I/D/msys64/mingw32/i686-w64-mingw32/include \
   -I./include/ \
+  -L/D/msys64/mingw32/lib \
+	-L/D/msys64/mingw32/i686-w64-mingw32/lib \
+	-L$THIRD_PARTY_PATH/mingw32/lib \
   -L./ \
   -L/usr/local/lib \
   -L/usr/lib \
   -lpthread -ldl \
   -lz -lstdc++ -lm \
-	-lpthread -lws2_32 -ldl -llzma -lbz2 \
+	-lws2_32 -llzma -lbz2 \
 	-limagehlp -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 \
 	-loleaut32 -lshell32 -lversion -luuid -lOpenGL32 \
 	-lwinspool -lcomctl32 -lcomdlg32 -lctl3d32 -ladvapi32 -lwsock32 -lkernel32 -luser32 \
